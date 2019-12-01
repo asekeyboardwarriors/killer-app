e6cf0adc53036b9be0e175bf11a955a27801f2d1
tashinam <tashinamgyal@mac.com>
2019-12-01 10:41:43 +0000

    - Installed heatmap.js: this required modifying the angular.json scripts object.
    - Installed @types files for heatmap.js and leaflet.js but there was a problem...
    heatmap.js had a node_modules subfolder with a different version of leaflet.js
    which had to be deleted, and the @types/heatmap.js folder had the same.
    Deleting this didn't work though, as the version was 0.7.33 instead of 1.5.5.
    This types file didn't have any types for ILayers so I had to copy and paste
    the relevant lines, which I'm sure is bad practise because you'll have to do the
    same to get it to work...
    
    - Modified home.page.html to change MapLayer to MapLayers
    
    - added 4 icons to assets
    
    - Several changes in home.page.ts:
    	- Imported heatmap.js and a few other things from 'leaflet'
    	- created some new variables in the constructor (without types - bad practice?
    	- created four markers with different icons and added them to a layergroup
    	- moved the creation of baselayer outside of options for readability
    	- added a configuration for the heatmap
    	- added some test data points
    	- added a heatmap layer and set the data to the test data
    	- updated the layersControl overlays from shapes to the new markers and heatmap
    	- added the heatmaplayer to onMapReady so displays by default

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { LeafletControlLayersConfig } from '@asymmetrik/ngx-leaflet';
 import { GeolocationPosition } from '@capacitor/core';
-import { circle, LatLng, latLng, Layer, LayerGroup, Map, MapOptions, polygon, tileLayer } from 'leaflet';
+import 'heatmap.js';
+import { circle, icon, LatLng, latLng, Layer, layerGroup, LayerGroup, Map, MapOptions, marker, polygon, tileLayer } from 'leaflet';
 import { Subscription } from 'rxjs';
 import { GeoLocationService } from '../services/GeoLocation/geo-location.service';
 import { SettingsService } from '../services/Settings/settings.service';
-
 
 @Component({
     selector: 'app-home',
@@ -15,33 +15,88 @@ import { SettingsService } from '../services/Settings/settings.service';
 
 export class HomePage {
     center: LatLng = latLng(46.879966, -121.726909);
+    testData;
+    cfg;
+    heatmapLayer;
+    houses;
+    base;
     options: MapOptions;
     mapLayers: LayerGroup = new LayerGroup<Layer>();
     isLoading = false;
     map: Map;
     layersControl: LeafletControlLayersConfig;
-
     private _subs: Subscription = new Subscription();
-
     constructor(private userLoc: GeoLocationService,
                 private userSettings: SettingsService) {
+
+        const detached = marker([50.8408, -0.1728], {
+            icon: icon({
+                iconSize: [ 20, 20 ],
+                iconAnchor: [ 10, 10 ],
+                iconUrl: 'assets/icon/detached.png'
+            })
+        }).bindPopup('This is a Detached House');
+        const semidetached    = marker([50.838, -0.1], {
+            icon: icon({
+                iconSize: [ 20, 20 ],
+                iconAnchor: [ 10, 10 ],
+                iconUrl: 'assets/icon/semidetached.png'
+            })
+        }).bindPopup('This is Semi-Detached House');
+        const apartment    = marker([50.868, -0.08], {
+            icon: icon({
+                iconSize: [ 20, 20 ],
+                iconAnchor: [ 10, 10 ],
+                iconUrl: 'assets/icon/apartment.png'
+            })
+        }).bindPopup('This is an Apartment');
+        const terrace    = marker([50.858, -0.11], {
+            icon: icon({
+                iconSize: [ 20, 20 ],
+                iconAnchor: [ 10, 10 ],
+                iconUrl: 'assets/icon/terrace.png'
+            })
+        }).bindPopup('This is a Terraced House');
+
+        this.houses = layerGroup([detached, semidetached, apartment, terrace]);
+        this.base = tileLayer('https://api.mapbox.com/styles/v1/avalothoath/{id}/tiles/256/{z}/{x}/{y}?access_token={accessToken}',{
+            attribution: '',
+            maxZoom: 18,
+            id: 'ck2w16ogc18le1co489mva17t',
+            accessToken: 'pk.eyJ1IjoiYXZhbG90aG9hdGgiLCJhIjoiY2sydWY1OWpvMHptMTNtcHZnbnl0YjB0MCJ9.Ryrcvt-TdL-vUH0SzMp2DQ'
+        });
         this.options = {
-            layers: [tileLayer('https://api.mapbox.com/styles/v1/avalothoath/{id}/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
-                attribution: '',
-                maxZoom: 18,
-                id: 'ck2w16ogc18le1co489mva17t',
-                accessToken: 'pk.eyJ1IjoiYXZhbG90aG9hdGgiLCJhIjoiY2sydWY1OWpvMHptMTNtcHZnbnl0YjB0MCJ9.Ryrcvt-TdL-vUH0SzMp2DQ'
-            })],
+            layers: [this.base],
             zoom: 10,
             center: this.center
         };
+        this.cfg = ({
+            radius: 20,
+            maxOpacity: 0.8,
+            scaleRadius: false,
+            useLocalExtrema: true,
+            blur: 0.95,
+            latField: 'lat',
+            lngField: 'lng',
+            valueField: 'count',
+            gradient: {
+                '.5': 'white',
+                '.8': 'pink',
+                '.95': 'red'
+            }
+        });
+        this.testData = {
+            data: [{lat: 50.8408, lng: -0.1728, count: 2}, {lat: 50.838, lng: -0.1, count: 1}, {lat: 50.868, lng: -0.08, count: 2}, {lat: 50.858, lng: -0.11, count: 3}]
+        };
+        this.heatmapLayer = new HeatmapOverlay(this.cfg);
+        this.heatmapLayer.setData(this.testData);
         this.layersControl = {
             baseLayers: {},
             overlays: {
-                'Big Circle': circle([ 46.95, -122 ], { radius: 5000 }),
-                'Big Square': polygon([[ 46.8, -121.55 ], [ 46.9, -121.55 ], [ 46.9, -121.7 ], [ 46.8, -121.7 ]])
+                'Heatmap': this.heatmapLayer,
+                'House Icons': this.houses
             }
-        }
+        };
 
     }
 
@@ -62,6 +117,7 @@ export class HomePage {
 
     onMapReady(map: Map): void {
         this.map = map;
+        this.map.addLayer(this.heatmapLayer)
         this.userLoc.currentLocation()
             .then((geoObject: GeolocationPosition) => {
                 this.map.panTo(latLng(geoObject.coords.latitude, geoObject.coords.longitude));
