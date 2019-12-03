c84f26ed5a3f3780018df4b2da1b57e66508e9e8
Pitsillides Charalambos <harryspitsillides@hotmail.com>
2019-12-03 18:59:32 +0000

    Changed file director

--- build-scripts/cleanup.js
+++ build-scripts/cleanup.js
@@ -1,9 +1,5 @@
 const fs = require('fs');
 const path = 'node_modules/@types/leaflet';
 if (fs.existsSync(path)) {
-    try {
-        fs.unlinkSync(path)
-    } catch (e) {
-        console.log(e);
-    }
+    fs.unlinkSync(path)
 }
