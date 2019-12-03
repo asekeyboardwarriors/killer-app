2fb820ababdfda5dced29d8b09cc8d11472bf653
HarryPi <harryspitsillides@hotmail.com>
2019-12-03 16:32:20 +0000

    Do not create non working spec files

--- src/app/Models/properties/properties-model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { PropertiesModel } from './properties-model';
-
-describe('PropertiesModel', () => {
-  it('should create an instance', () => {
-    expect(new PropertiesModel()).toBeTruthy();
-  });
-});
