354679e49cdf26087cc7b9cbc4d7bde39147e11a
dominikwasilewski <dominikwas@outlook.com>
2019-12-04 10:52:16 +0000

    Removed an extra comma on auth page. Added the use of real data in the table. Changed colour of toolbar to the application theme. (properties-in-range.page.html) Added a property model with subscription that calls random properties and a loading indicator. (properties-in-range.page.ts)

--- src/app/properties-in-range/properties-in-range.page.ts
+++ src/app/properties-in-range/properties-in-range.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+import { Subject, Subscription } from 'rxjs';
+import { PropertyModel } from '../Models/properties/property-model';
+import { PropertiesService } from '../services/properties/properties.service';
 
 export interface PropertyArray {
   price: number;
@@ -35,5 +39,32 @@ const ELEMENT_DATA: PropertyArray[] = [
 export class PropertiesInRangePage {
 
   displayedColumns: Array<string> = ['price', 'date', 'housetype', 'duration', 'address'];
-  propertyArray = ELEMENT_DATA;
+  allPropertiesInRange: PropertyModel[];
+
+  private _sub: Subscription;
+  private _loadingIndicator: HTMLIonLoadingElement;
+
+  constructor(private _propertiesService: PropertiesService,
+              private _loader: LoadingController) {
+  }
+
+  ionViewWillEnter(): void {
+    this._loader.create({
+      message: 'Please wait :D!'
+    }).then( async overlay => {
+      this._loadingIndicator = overlay;
+      await overlay.present();
+    });
+    this._propertiesService.getRandomProperties()
+        .subscribe(async (propertiesInRange: PropertyModel[]) => {
+          this.allPropertiesInRange = propertiesInRange;
+          await this._loadingIndicator.dismiss();
+        }, async error => {
+          await this._loadingIndicator.dismiss();
+        });
+  }
+
+  ionViewWillLeave(): void {
+    this._sub.unsubscribe();
+  }
 }
